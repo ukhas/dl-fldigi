68c7ca6fb47b9ba3657d907587f97b3032af795a
Robert Harrison <robert@robert-laptop.(none)>
2010-03-22 21:22:23 +0000

    tidy up of the code

--- src/misc/dl_fldigi.cxx
+++ src/misc/dl_fldigi.cxx
@@ -4,25 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// This is the writer call back function used by curl  
-static int writer(char *data, size_t size, size_t nmemb, std::string *buffer)  
- {  
-   // What we will return  
-   int result = 0;  
-   
-   // Is there anything in the buffer?  
-   if (buffer != NULL)  
-   {  
-     // Append the data to the buffer  
-     buffer->append(data, size * nmemb);  
-   
-     // How much did we write?  
-     result = size * nmemb;  
-   }  
-   
-   return result;  
-} 
-
 void dlServerCommunicator()
 {
 	if (pipe(dl_fldigi_pfds) == -1) { perror("pipe"); exit(EXIT_FAILURE); }
@@ -30,27 +11,25 @@ void dlServerCommunicator()
 	dl_fldigi_cpid = fork();
 
 	if (dl_fldigi_cpid == -1) { perror("fork"); exit(EXIT_FAILURE); }
-	if (dl_fldigi_cpid == 0) {
+
+	if (dl_fldigi_cpid == 0) // Test to see if this is the child process
+	{
+
+		/* 
+		    Code to be excuted by child process
+                */
 
 		char c;
 		char buffer [2000];
 		int charpos = 0;
 
+		/* Initialise the pipe */
+		buffer[0] ='\0';
+
+		close(dl_fldigi_pfds[1]); // Close the write side of the pipe 
+
 		CURL *easyhandle_status;
 		CURLcode result;
-
-		// ofstream fout;
-
-		// fout.clear();
-		// fout.open ("log.txt");
-		// if (fout.fail()) {
-		// 	cout << "Failed to open log.txt\n";
-		// }
-	
-		/* Close the write side of the pipe */
-		close(dl_fldigi_pfds[1]);
-
-		buffer[0] ='\0';
 
 		while (read(dl_fldigi_pfds[0], &c, 1) > 0)
 		{
@@ -69,18 +48,19 @@ void dlServerCommunicator()
 
 				printf(" CHILD: received \"%s\"\n", buffer);
 
-				// fout << "Buffer string : '" << buffer << "'" << endl;
-
-				if (strlen(buffer) > 0)
+				if (strlen(buffer) > 0) // Only post data if there is some
 				{
 					easyhandle_status = curl_easy_init();
-					if(easyhandle_status) {
+
+					if(easyhandle_status) 
+					{
 						curl_easy_setopt(easyhandle_status, CURLOPT_POSTFIELDS, buffer);
 						curl_easy_setopt(easyhandle_status, CURLOPT_URL, "http://www.robertharrison.org/listen/listen.php");
 						result = curl_easy_perform(easyhandle_status); /* post away! */
 						std::cout << "result: " << result  << "\n";
 						curl_easy_cleanup(easyhandle_status);
 					}
+
 				}
 				buffer[0] = '\0';
 			}
@@ -89,6 +69,9 @@ void dlServerCommunicator()
 		_exit (0);
         } 
 	
-	/* Close the read side of the pipe */
-	close(dl_fldigi_pfds[0]);
+	/* 
+	    Code to be excuted by parent process
+        */
+	close(dl_fldigi_pfds[0]); // Close the read side of the pipe 
+
 }
