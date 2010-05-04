c401c35256d0d2d9e9e90d9902ec669d00b869c7
Daniel Richman <main@danielrichman.co.uk>
2010-05-04 20:54:56 +0100

    Fixed hamlib-static linking (MINGW)

--- scripts/mkhamlibstatic.sh
+++ scripts/mkhamlibstatic.sh
@@ -42,4 +42,4 @@ case "$target_os" in
 	;;
 esac
 
-$CXX -o ${1}${EXEEXT} $AM_CXXFLAGS $fldigi_CXXFLAGS $CXXFLAGS $AM_LDFLAGS $fldigi_LDFLAGS $LDFLAGS $fldigi_OBJECTS $fldigi_LDADD
+$CXX -o ${1}${EXEEXT} $AM_CXXFLAGS $dl_fldigi_CXXFLAGS $CXXFLAGS $AM_LDFLAGS $dl_fldigi_LDFLAGS $LDFLAGS $dl_fldigi_OBJECTS $dl_fldigi_LDADD
