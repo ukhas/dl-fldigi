70b3dab9fc457a8ed8684aa5d77e36ba6fcb83d3
Philip Heron <phil@sanslogic.co.uk>
2010-10-13 16:41:58 +0100

    Flush any remaining bits left in the output bit buffer

--- src/ssdv/ssdv.c
+++ src/ssdv/ssdv.c
@@ -361,7 +361,11 @@ static char ssdv_process(ssdv_t *s)
 			s->mcu_id++;
 			
 			/* Test for the end of image */
-			if(s->mcu_id >= s->mcu_count) return(SSDV_EOI);
+			if(s->mcu_id >= s->mcu_count)
+			{
+				ssdv_outbits_sync(s);
+				return(SSDV_EOI);
+			}
 			
 			/* Set the packet MCU marker - encoder only */
 			if(s->packet_mcu_id == 0xFFFF)
@@ -898,7 +902,6 @@ char ssdv_dec_get_jpeg(ssdv_t *s, uint8_t **jpeg, size_t *length)
 	if(s->mcu_id < s->mcu_count) ssdv_fill_gap(s, s->mcu_count);
 	
 	/* Sync, and final EOI header and return */
-	ssdv_outbits_sync(s);
 	s->out_stuff = 0;
 	ssdv_write_marker(s, J_EOI, 0, 0);
 	
