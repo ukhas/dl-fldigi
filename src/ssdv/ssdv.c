d5902a94b37829d1d14db885188deea093c1a7de
Philip Heron <phil@sanslogic.co.uk>
2011-06-25 00:15:42 +0100

    Don't crash on a marker error, it's rude

--- src/ssdv/ssdv.c
+++ src/ssdv/ssdv.c
@@ -582,7 +582,8 @@ char ssdv_enc_get_packet(ssdv_t *s)
 			{
 				/* Marker without data */
 				s->marker_len = 0;
-				ssdv_have_marker(s);
+				r = ssdv_have_marker(s);
+				if(r != SSDV_OK) return(r);
 			}
 			else if(s->marker >= J_SOF0 && s->marker <= J_COM)
 			{
@@ -598,7 +599,8 @@ char ssdv_enc_get_packet(ssdv_t *s)
 			if((s->needbits -= 8) == 0)
 			{
 				s->marker_len -= 2;
-				ssdv_have_marker(s);
+				r = ssdv_have_marker(s);
+				if(r != SSDV_OK) return(r);
 			}
 			break;
 		
@@ -606,7 +608,8 @@ char ssdv_enc_get_packet(ssdv_t *s)
 			s->marker_data[s->marker_data_len++] = b;
 			if(s->marker_data_len == s->marker_len)
 			{
-				ssdv_have_marker_data(s);
+				r = ssdv_have_marker_data(s);
+				if(r != SSDV_OK) return(r);
 			}
 			break;
 		
