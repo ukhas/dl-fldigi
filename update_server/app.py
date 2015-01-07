cb85bd79502d8e06232eacee70932188d8e2995c
Adam Greig <adam@adamgreig.com>
2015-01-07 22:00:27 +0000

    check based on all old git IDs instead

--- update_server/app.py
+++ update_server/app.py
@@ -1,46 +1,54 @@
 #!/usr/bin/env python
 
 # Copyright 2012 Daniel Richman
+# Copyright 2015 Adam Greig
 # DL-Fldigi update check server
 
-import os.path
+import os
 import yaml
+import os.path
+import subprocess
 from flask import Flask, abort, request, jsonify
-
-app_dir = os.path.dirname(__file__)
-config_file = os.path.join(app_dir, "config.yml")
-config = {}
 
 app = Flask(__name__)
 
-def load_config():
-    global config
+# Load config from file
+app_dir = os.path.abspath(os.path.dirname(__file__))
+config_file = os.path.join(app_dir, "config.yml")
+config = yaml.load(open(config_file))
 
-    mtime = os.stat(config_file).st_mtime
-    if mtime != config.get("_mtime", None):
-        with open(config_file) as f:
-            config = yaml.load(f)
-        config["_mtime"] = mtime
+# Swap to directory containing this script, to ensure we're inside
+# the git repository.
+os.chdir(app_dir)
+
+
+def git_rev_parse(name):
+    arg = name + "^{}"
+    commit = subprocess.check_output(["git", "rev-parse", "--verify", arg])
+    return commit.strip()
+
+
+def git_rev_list(commit):
+    commits = subprocess.check_output(["git", "rev-list", commit])
+    return set(commits.split("\n")[1:])
+
+# Store commits considered old
+old_commits = git_rev_list(git_rev_parse(config['latest_release']))
+
 
 @app.route("/")
 def check():
-    load_config()
-
     try:
-        platform = request.args["platform"]
         commit = request.args["commit"]
-        expect = config["expect"][platform]
 
-        if isinstance(expect, list) and commit in expect:
-            return ""
-        elif isinstance(expect, basestring) and expect == commit:
-            return ""
-        else:
+        if commit in old_commits:
             return jsonify(config["update"])
+        else:
+            return ""
 
     except KeyError:
         # bad platform or missing arg
         abort(400)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
